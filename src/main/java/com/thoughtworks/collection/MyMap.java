1615f99d58fd276f6986dbfbcbb6dd048a9516c9
willow <ylmap@126.com>
2018-05-01 12:50:30 +0800

    MyMap.java

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MyMap {
 
@@ -13,26 +12,75 @@ public class MyMap {
     private List<String> letterList = Arrays.asList(letters);
 
     public MyMap(List<Integer> array) {
+
         this.array = array;
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> arrayList = new ArrayList();
+
+        for(int element:this.array)
+        {
+                arrayList.add(element*3);
+        }
+
+        this.array=arrayList;
+        return this.array;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> arrayList = new ArrayList();
+
+        for(int index:this.array)
+        {
+            arrayList.add(this.letterList.get(index-1));
+        }
+
+        return arrayList;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> arrayList = new ArrayList();
+
+        for(int index:this.array)
+        {
+            if(index>=26) {
+                int firstind = index / 26;
+                int secondind = index % 26;
+                 if (secondind == 0) {
+                    arrayList.add(letterList.get(firstind - 2) + "z");
+                } else {
+                    arrayList.add(letterList.get(firstind - 1) + letterList.get(secondind - 1));
+                }
+            }
+            else
+            {
+                arrayList.add(this.letterList.get(index - 1));
+            }
+        }
+
+        return arrayList;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        Integer[] arrayResult=new Integer[]{};
+        arrayResult= (Integer[]) this.array.toArray();
+
+        Arrays.sort(arrayResult);
+        List<Integer> result=Arrays.asList(arrayResult);
+        Collections.reverse(result);
+
+        return result;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        Integer[] arrayResult=new Integer[]{};
+        arrayResult= (Integer[]) this.array.toArray();
+
+        Arrays.sort(arrayResult);
+        List<Integer> result=Arrays.asList(arrayResult);
+
+        return result;
     }
+
 }
