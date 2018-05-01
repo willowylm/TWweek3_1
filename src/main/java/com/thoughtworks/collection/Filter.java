b6f196997002c179b8e58d5cbc4d0f82ae80e736
willow <ylmap@126.com>
2018-05-01 12:44:46 +0800

    Filter.java

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,30 +2,77 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
     List<Integer>  array;
 
     public Filter(List<Integer> array) {
-     this.array = array;
+        this.array = array;
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> arrayList = new ArrayList();
+
+        for(int element:this.array)
+        {
+            if(element%2==0)
+               arrayList.add(element);
+        }
+
+       this.array=arrayList;
+        return this.array;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        List<Integer> arrayList = new ArrayList();
+
+        for(int element:this.array)
+        {
+            if(element%3==0)
+                arrayList.add(element);
+        }
+
+        this.array=arrayList;
+        return this.array;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> arrayList = new ArrayList();
+
+        for(int firstListElement:firstList)
+        {
+            for(int secondListElement:secondList)
+            {
+                if(firstListElement == secondListElement)
+                {
+                    arrayList.add(firstListElement);
+                    break;
+                }
+            }
+        }
+
+       return arrayList;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> arrayList = new ArrayList();
+
+        for(int element:this.array)
+        {
+            if(arrayList.contains(element))
+                continue;
+            else
+                arrayList.add(element);
+        }
+        this.array=arrayList;
+        return this.array;
     }
+
 }
